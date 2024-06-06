9ac03de775e46ad972f9fe043182350577269aa9
Laurent-I <irakramlaw@gmail.com>
2024-06-06 11:41:22 +0200

    docs: added doc for all routes

--- routes/v1/docs.route.js
+++ routes/v1/docs.route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const specs = swaggerJsdoc({
   swaggerDefinition,
-  apis: ['src/docs/*.yml', 'src/routes/v1/*.js'],
+  apis: ['/docs/*.yml', '/routes/v1/*.js'],
 });
 
 router.use('/', swaggerUi.serve);
